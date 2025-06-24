a651e9e043bd195757391c59fe1954f3322750ec
The Bit Dev <ryanvincent.capio@outlook.com>
2025-06-25 01:00:32 +0400

    added eslint node

---
+++ packages/eslint-config/node.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import perfectionist from "eslint-plugin-perfectionist";
+import { config as baseConfig } from "./base.js";
+
+export const nodeJsConfig = [
+  ...baseConfig,
+  tseslint.config(
+    {
+      ignores: ["**/*.js"],
+    },
+    eslint.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    {
+      languageOptions: {
+        parserOptions: {
+          projectService: true,
+          tsconfigRootDir: import.meta.dirname,
+        },
+      },
+    }
+  ),
+  perfectionist.configs["recommended-natural"],
+];
