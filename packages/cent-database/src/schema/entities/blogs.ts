ea37c7efbdcab7a18b4ddd08d39d843843b9335f
The Bit Dev <ryanvincent.capio@outlook.com>
2025-06-25 23:23:23 +0400

    used shadcn for components

--- packages/cent-database/src/schema/entities/blogs.ts
+++ packages/cent-database/src/schema/entities/blogs.ts
@@ -1,13 +1,14 @@
 import { jsonb, pgTable, text } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+
 import { baseModelWithOwner } from "../abstract/baseModelWithOwner";
 
 export const blogsTable = pgTable("blogs", {
   ...baseModelWithOwner,
+  content: text("content").notNull(),
+  meta: jsonb("meta").$type<Record<string, unknown>>().default({}),
   slug: text("slug").notNull().unique(),
   title: text("title").notNull(),
-  content: text("content").notNull(),
-  meta: jsonb("meta").$type<Record<string, any>>().default({}),
 });
 
 export const blogSelectSchema = createSelectSchema(blogsTable);
