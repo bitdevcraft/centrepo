4db448c8d0549afb02a33b382033fc3d9388c31a
The Bit Dev <ryanvincent.capio@outlook.com>
2025-06-28 20:56:57 +0400

    added cent-auth

--- packages/cent-auth/tsup.config.ts
+++ packages/cent-auth/tsup.config.ts
@@ -5,8 +5,6 @@ export default defineConfig({
   dts: true,
   entry: {
     index: "src/index.ts",
-    repositories: "src/repositories/index.ts",
-    schema: "src/schema/index.ts",
   },
   format: ["cjs", "esm"],
   sourcemap: true,
